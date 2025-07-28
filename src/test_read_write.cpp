a6ec8fccf2bc09f3c126486ddd6cc6dc37c51605
Vũ Tân Hưng <agv04@rtc.edu.vn>
2025-07-28 21:37:47 +0700

    fix log

--- src/test_read_write.cpp
+++ src/test_read_write.cpp
@@ -21,13 +21,19 @@ int main(int argc, char **argv) {
     while (rclcpp::ok()) {
       for (auto &[name, address] : kCoils) {
         uint8_t data[3];
-        node->read_coils(address, 1, data);
+        if (!node->read_coils(address, 1, data)) {
+          RCLCPP_ERROR(kLogger, "Failed to read coils");
+          continue;
+        }
         RCLCPP_INFO(kLogger, "%s: %02X", name.c_str(), data[0] & 0x01);
       }
 
       for (auto &[name, address] : kHoldingRegisters) {
         uint16_t data[3];
-        node->read_holding_registers(address, 1, data);
+        if (!node->read_holding_registers(address, 1, data)) {
+          RCLCPP_ERROR(kLogger, "Failed to read holding registers");
+          continue;
+        }
         RCLCPP_INFO(kLogger, "%s: %04X", name.c_str(), data[0]);
       }
 
